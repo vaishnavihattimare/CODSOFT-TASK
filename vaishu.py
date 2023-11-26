46c89291ec0856e2689cccb753d9495e6d4f3fab
vaishnavihattimare <152077370+vaishnavihattimare@users.noreply.github.com>
2023-11-26 19:37:01 +0530

    Add files via upload

---
+++ vaishu.py
@@ -0,0 +1,46 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    return a / b
+
+print("Enter an operation.")
+print("1.Addition")
+print("2.Subtraction")
+print("3.Multiplication")
+print("4.Division")
+
+while True:
+    choice = input("Enter choice(1/2/3/4): ")
+
+    if choice in ('1', '2', '3', '4'):
+        try:
+            n1 = float(input("Enter first number: "))
+            n2 = float(input("Enter second number: "))
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+            continue
+
+        if choice == '1':
+            print(n1, "+", n2, "=", add(n1, n2))
+
+        elif choice == '2':
+            print(n1, "-", n2, "=", subtract(n1, n2))
+
+        elif choice == '3':
+            print(n1, "*", n2, "=", multiply(n1, n2))
+
+        elif choice == '4':
+            print(n1, "/", n2, "=", divide(n1, n2))
+        
+        next_calculation = input("Let's do next calculation? (yes/no): ")
+        if next_calculation == "no":
+          break
+    else:
+        print("Invalid Input")
